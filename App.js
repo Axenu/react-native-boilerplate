f309199faa26d933a2a895435dadfc11d00cdc0a
Axenu <simonseregon@gmail.com>
2019-04-28 00:57:45 +0200

    Firebase basic config with notifications

--- App.js
+++ App.js
@@ -9,12 +9,53 @@
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 import { createAppContainer } from "react-navigation";
+import firebase from "react-native-firebase";
+import { AsyncStorage } from "react-native";
+
 import { AppNavigator } from "./src/config/navigator";
 
 const AppContainer = createAppContainer(AppNavigator);
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentDidMount() {
+    this.checkPermission();
+  }
+
+  //1
+  async checkPermission() {
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      this.getToken();
+    } else {
+      this.requestPermission();
+    }
+  }
+
+  //3
+  async getToken() {
+    let fcmToken = await AsyncStorage.getItem("fcmToken");
+    if (!fcmToken) {
+      fcmToken = await firebase.messaging().getToken();
+      if (fcmToken) {
+        // user has a device token
+        await AsyncStorage.setItem("fcmToken", fcmToken);
+      }
+    }
+  }
+
+  //2
+  async requestPermission() {
+    try {
+      await firebase.messaging().requestPermission();
+      // User has authorised
+      this.getToken();
+    } catch (error) {
+      // User has rejected permissions
+      console.log("permission rejected");
+    }
+  }
+
   render() {
     return <AppContainer />;
   }
---
+++ ios/Pods/Target Support Files/Pods-MickeGTests/Pods-MickeGTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MickeGTests : NSObject
+@end
+@implementation PodsDummy_Pods_MickeGTests
+@end
